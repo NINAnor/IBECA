bb3b9374975e43943c4255f65e5bae61f329abe4
ninsbl <stefan.blumentrath@gmx.de>
2021-12-01 12:04:08 +0100

    utkast fjell avstand indikato

---
+++ data/fjell_trussel_dist_stats.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+r.external input=data/fjellmasken.tif output=fjellmasken --o --v
+v.import input="/home/NINA.NO/stefan.blumentrath/IBECA/data/regioner_2010/regNorway_wgs84 - MERGED.shp" layer="regNorway_wgs84 - MERGED" output="regNorway_wgs84___MERGED"
+eval `g.region -g raster=fjellmasken vector="regNorway_wgs84___MERGED" align=fjellmasken`
+
+gdal_rasterize -at -sql "SELECT geom FROM \"Topography\".\"Norway_N50_BygningsPunkt\"
+UNION ALL
+SELECT geom FROM \"Topography\".\"Norway_N50_JernbaneStasjon\"
+UNION ALL
+SELECT geom FROM \"Topography\".\"Norway_N50_Bane\"
+UNION ALL
+SELECT geom FROM \"Topography\".\"Norway_N50_VegSti\" WHERE \"OBJTYPE\" != 'Sti'
+UNION ALL
+SELECT geom FROM \"Topography\".\"Norway_N50_ArealdekkeFlate\" WHERE \"OBJTYPE\" IN (
+'Alpinbakke'
+,'BymessigBebyggelse'
+,'DyrketMark'
+,'Golfbane'
+,'Gravplass'
+,'Hyttefelt'
+,'Industriområde'
+,'Lufthavn'
+,'Park'
+,'Rullebane'
+,'Skog'
+,'SportIdrettPlass'
+,'Steinbrudd'
+,'Steintipp'
+,'TettBebyggelse'
+)" -burn 1 -of GTiff -a_srs EPSG:25833 -co COMPRESS=LZW -co PREDICTOR=2 -a_nodata 0 -te $w $s $e $n -tr $ewres $nsres -ot Byte -optim AUTO "PG:host=gisdata-db.nina.no dbname=gisdata user=stefan.blumentrath" /data/scratch/n50_fjell_trussler.tif
+r.external input=/data/scratch/n50_fjell_trussler.tif output=n50_fjell_trussler --o --v
+
+r.grow.distance --overwrite --verbose input=n50_fjell_trussler distance=n50_fjell_trussler_dist --o --v
+
+v.to.rast --overwrite --verbose input=regNorway_wgs84___MERGED type=point,line,centroid,area output=regNorway_wgs84___MERGED use=attr attribute_column=id memory=3000
+
+echo "0 = NULL
+* = 1" | r.reclass input=fjellmasken output=MASK rules=- --o --v
+
+r.univar -e -t --overwrite --verbose map=n50_fjell_trussler_dist zones=regNorway_wgs84___MERGED output=data/fjell_trussel_dist_stats.csv separator=comma
+
+# Clean up
+rm /data/scratch/n50_fjell_trussler.tif
